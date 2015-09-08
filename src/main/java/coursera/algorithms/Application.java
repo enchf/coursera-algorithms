db279fd5168ddf6f944417f9ae933a0b1ee94c33
enchf <e.ernesto.espinosa@gmail.com>
2015-09-08 18:32:31 -0500

    Added Spring Boot dependencies

---
+++ src/main/java/coursera/algorithms/Application.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package coursera.algorithms;
+
+/**
+ * @author nacho
+ *
+ */
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
